153b92f1e6092b10aa5949b158b06438f951c0c5
김민기 <rlaalsrl9569@naver.com>
2021-04-16 23:30:05 +0900

    Finish Bounty Rank App

---
+++ BountyList/BountyList/BountyInfo.swift
@@ -0,0 +1,22 @@
+//
+//  BountyInfo.swift
+//  BountyList
+//
+//  Created by 김민기 on 2021/04/15.
+//
+
+import UIKit
+
+struct BountyInfo {
+    let name: String
+    let bounty: Int
+    
+    var image: UIImage? {
+        return UIImage(named: "\(name).jpg")
+    }
+    
+    init(name: String, bounty: Int) {
+        self.name = name
+        self.bounty = bounty
+    }
+}
