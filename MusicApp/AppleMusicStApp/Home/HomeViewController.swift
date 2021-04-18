ebac4967fb18420dd5fc531617d64cd5f9ec0ec0
김민기 <rlaalsrl9569@naver.com>
2021-04-18 22:36:19 +0900

    add Home View and Player Veiw

--- MusicApp/AppleMusicStApp/Home/HomeViewController.swift
+++ MusicApp/AppleMusicStApp/Home/HomeViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class HomeViewController: UIViewController {
     // TODO: 트랙관리 객체 추가
-    
+    let trackManager: TrackManager = TrackManager()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,15 +21,18 @@ extension HomeViewController: UICollectionViewDataSource {
     // 몇개 표시 할까?
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         // TODO: 트랙매니저에서 트랙갯수 가져오기
-        return 10
+        return trackManager.tracks.count
     }
     
     // 셀 어떻게 표시 할까?
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         // TODO: 셀 구성하기
-        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "TrackCollectionViewCell", for: indexPath) as? TrackCollectionViewCell else {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "TrackCollecionViewCell", for: indexPath) as? TrackCollecionViewCell else {
             return UICollectionViewCell()
         }
+        
+        let track = trackManager.track(at: indexPath.item)
+        cell.updateUI(item: track)
         return cell
     }
     
@@ -37,8 +40,22 @@ extension HomeViewController: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
         switch kind {
         case UICollectionView.elementKindSectionHeader:
+            guard let item = trackManager.todaysTrack else {
+                return UICollectionReusableView()
+            }
+            
+            guard let header = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: "TrackCollectionHeaderView", for: indexPath) as? TrackCollectionHeaderView else {
+                return UICollectionReusableView()
+            }
+            
+            header.update(with: item)
+            header.tapHandler = { item -> Void in
+                // Player를 띄운다.
+                print("---> item title: \(item.convertToTrack()?.title)")
+            }
+            
             // TODO: 헤더 구성하기
-            return UICollectionReusableView()
+            return header
         default:
             return UICollectionReusableView()
         }
@@ -49,6 +66,7 @@ extension HomeViewController: UICollectionViewDelegate {
     // 클릭했을때 어떻게 할까?
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         // TODO: 곡 클릭시 플레이어뷰 띄우기
+        let playerStoryboard = UIStoryboard.init(name: "player", bundle: nil)
     }
 }
 
@@ -59,9 +77,8 @@ extension HomeViewController: UICollectionViewDelegateFlowLayout {
         // TODO: 셀사이즈 구하기
         let itemSpacing: CGFloat = 20
         let margin: CGFloat = 20
-        let width = (collectionView.bounds.width - itemSpacing - margin * 2)
+        let width = (collectionView.bounds.width - itemSpacing - margin * 2) / 2
         let height = width + 60
-        
         return CGSize(width: width, height: height)
     }
 }
--- MusicApp/AppleMusicStApp/Home/TrackCollectionViewCell.swift
+++ MusicApp/AppleMusicStApp/Home/TrackCollectionViewCell.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class TrackCollectionViewCell: UICollectionViewCell {
+class TrackCollecionViewCell: UICollectionViewCell {
     @IBOutlet weak var trackThumbnail: UIImageView!
     @IBOutlet weak var trackTitle: UILabel!
     @IBOutlet weak var trackArtist: UILabel!
@@ -21,6 +21,9 @@ class TrackCollectionViewCell: UICollectionViewCell {
     
     func updateUI(item: Track?) {
         // TODO: 곡정보 표시하기
-        
+        guard let track = item else { return }
+        trackThumbnail.image = track.artwork
+        trackTitle.text = track.title
+        trackArtist.text = track.artist
     }
 }
