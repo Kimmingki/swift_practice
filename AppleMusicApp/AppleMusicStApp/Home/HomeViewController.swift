c893e38daa0ca19109046e8f00a93ae144b5e14c
Minki <rlaalsrl9569@naver.com>
2021-06-29 09:05:52 +0900

    플레이어 띄우기, 곡정보 표시 및 재생 구현, 재생시간 실시간 처리

--- AppleMusicApp/AppleMusicStApp/Home/HomeViewController.swift
+++ AppleMusicApp/AppleMusicStApp/Home/HomeViewController.swift
@@ -52,7 +52,11 @@ extension HomeViewController: UICollectionViewDataSource {
             header.update(with: item)
             header.tapHandler = { item -> Void in
                 // player 띄우기
-                print("---> item \(item.convertToTrack()?.title)")
+                let playerStoryboard = UIStoryboard.init(name: "Player", bundle: nil)
+                guard let playerVC = playerStoryboard.instantiateViewController(identifier: "PlayerViewController") as? PlayerViewController else { return }
+                // 곡 전달하기
+                playerVC.simplePlayer.replaceCurrentItem(with: item)
+                self.present(playerVC, animated: true, completion: nil)
             }
             
             // TODO: 헤더 구성하기
@@ -67,6 +71,12 @@ extension HomeViewController: UICollectionViewDelegate {
     // 클릭했을때 어떻게 할까?
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         // TODO: 곡 클릭시 플레이어뷰 띄우기
+        let playerStoryboard = UIStoryboard.init(name: "Player", bundle: nil)
+        guard let playerVC = playerStoryboard.instantiateViewController(identifier: "PlayerViewController") as? PlayerViewController else { return }
+        // 곡 전달하기
+        let item = trackManager.tracks[indexPath.item]
+        playerVC.simplePlayer.replaceCurrentItem(with: item)
+        present(playerVC, animated: true, completion: nil)
     }
 }
 
