96280d6099076d18177e03231356d2956eaa6a19
Minki <rlaalsrl9569@naver.com>
2021-07-09 19:12:59 +0900

    URLSession

---
+++ networking/URLSession_Basic.playground/Contents.swift
@@ -0,0 +1,38 @@
+import UIKit
+
+// URLSession
+
+// 1. URLSessionConfiguration
+// 2. URLSession
+// 3. URLSessionTask를 이용해서 서버와 네트워킹
+
+// URLSessionTask
+
+// - dataTask
+// - uploadTask
+// - downloadTask
+
+
+let config = URLSessionConfiguration.default
+let session = URLSession(configuration: config)
+
+
+// URL Components
+var urlComponents = URLComponents(string: "https://itunes.apple.com/search?")!
+let mediaQuery = URLQueryItem(name: "media", value: "music")
+let entityQuery = URLQueryItem(name: "entity", value: "song")
+let termQuery = URLQueryItem(name: "term", value: "Gdragon")
+urlComponents.queryItems?.append(mediaQuery)
+urlComponents.queryItems?.append(entityQuery)
+urlComponents.queryItems?.append(termQuery)
+let requestURL = urlComponents.url!
+
+
+let dataTask = session.dataTask(with: requestURL) { (data, response, error) in
+    guard error == nil else { return }
+    
+    guard let statusCode = (response as? HTTPURLResponse)?.statusCode else { return }
+    let successRange = 200..<300
+}
+
+
