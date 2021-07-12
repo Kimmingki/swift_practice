c9019e741626d1f9004d6ed9fd9398fb7c05870f
Minki <rlaalsrl9569@naver.com>
2021-07-12 22:27:52 +0900

    SearchAPI 구현 완료

--- MyNetflix/MyNetflix/SearchViewController.swift
+++ MyNetflix/MyNetflix/SearchViewController.swift
@@ -42,6 +42,7 @@ extension SearchViewController: UISearchBarDelegate {
         
         SearchAPI.search(searchTerm) { movies in
             // CollectionView로 표현하기
+            print("--> resultCount: \(movies.count)")
         }
         
         print("---> 검색어 : \(searchTerm)")
@@ -52,7 +53,6 @@ extension SearchViewController: UISearchBarDelegate {
 class SearchAPI {
     static func search(_ term: String, completion: @escaping ([Movie]) -> Void) {
         let session = URLSession(configuration: .default)
-        
         var urlComponents = URLComponents(string: "https://itunes.apple.com/search?")!
         let mediaQuery = URLQueryItem(name: "media", value: "movie")
         let entityQuery = URLQueryItem(name: "entity", value: "movie")
@@ -65,7 +65,6 @@ class SearchAPI {
         
         let dataTask = session.dataTask(with: requestURL) { data, response, error in
             let successRange = 200..<300
-            
             guard error == nil, let statusCode = (response as? HTTPURLResponse)?.statusCode,
                   successRange.contains(statusCode) else {
                 completion([])
@@ -78,21 +77,46 @@ class SearchAPI {
             }
             
             // data -> [Movie]
-            let string = String(data: resultData, encoding: .utf8)
-            print("---> result: \(string)")
-            
-//            completion([Movie])
-            
+            let movies = SearchAPI.parseMovies(resultData)
+            completion(movies)
         }
         dataTask.resume()
+    }
+    
+    static func parseMovies(_ data: Data) -> [Movie] {
+        let decoder = JSONDecoder()
+        do {
+            let response = try decoder.decode(Response.self, from: data)
+            let movies = response.movies
+            return movies
+        } catch let error {
+            print("--> parsing error: \(error.localizedDescription)")
+            return []
+        }
     }
 }
 
 
 struct Movie: Codable {
+    let title: String
+    let director: String
+    let thumbnailPath: String
+    let previewURL: String
     
+    enum CodingKeys: String, CodingKey {
+        case title = "trackName"
+        case director = "artistName"
+        case thumbnailPath = "artworkUrl100"
+        case previewURL = "previewUrl"
+    }
 }
 
 struct Response: Codable {
+    let resultCount: Int
+    let movies: [Movie]
     
+    enum CodingKeys: String, CodingKey {
+        case resultCount
+        case movies = "results"
+    }
 }
