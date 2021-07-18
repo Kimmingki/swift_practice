b5ea86cb4dca62abf099a916bb957367aeb65463
Minki <rlaalsrl9569@naver.com>
2021-07-18 19:04:20 +0900

    CocoaPods, iOS SDK 설치

---
+++ Firebase101/Firebase101/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Firebase101
+//
+//  Created by 김민기 on 2021/07/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
